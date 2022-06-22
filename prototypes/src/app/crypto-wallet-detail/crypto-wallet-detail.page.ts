ba21acfe6e552cfaa3304e38b960d1ba4d8d51ad
chokchai <chokchai9900@gmail.com>
2022-06-22 19:00:42 +0700

    edited wallet detail page

--- prototypes/src/app/crypto-wallet-detail/crypto-wallet-detail.page.ts
+++ prototypes/src/app/crypto-wallet-detail/crypto-wallet-detail.page.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/quotes */
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CryptoWalletDetailPage implements OnInit {
 
+  public data: any[] = [];
+  public templates: ListInfo[] = [
+    new ListInfo("เติมเงิน", "success","","assets/imgs/transaction-deposit.png"),
+    new ListInfo("ถอนเงิน", "danger","","assets/imgs/transaction-withdraw.png"),
+    new ListInfo("ซื้อของ", "primary","","assets/imgs/transaction-shop2.png"),
+    new ListInfo("โอนเงิน", "danger","","assets/imgs/transaction-out.png"),
+    new ListInfo("เงินเข้าบัญชี", "success","","assets/imgs/transaction-in.png"),
+  ];
+
+
   constructor() { }
 
   ngOnInit() {
+    this.data = [
+      {
+        title: "Crypto wallet pages list",
+        data: this.templates
+      },
+    ];
   }
 
+}
+
+export class ListInfo {
+  constructor(
+    private title: string = "",
+    private logType: string = "",
+    private link: string = "",
+    private imgLink: string = ""
+    ) { }
 }
